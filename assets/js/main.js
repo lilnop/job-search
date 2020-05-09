0b186b27514e61c688397ab8ae1a3987c11ce9f0
Robert Sarpong <34831325+lilnop@users.noreply.github.com>
2020-05-09 12:14:19 -0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+(function ($)
+  { "use strict"
+  
+/* 8. sildeBar scroll */
+    $.scrollUp({
+      scrollName: 'scrollUp', // Element ID
+      topDistance: '300', // Distance from top before showing element (px)
+      topSpeed: 300, // Speed back to top (ms)
+      animation: 'fade', // Fade, slide, none
+      animationInSpeed: 200, // Animation in speed (ms)
+      animationOutSpeed: 200, // Animation out speed (ms)
+      scrollText: '<i class="ti-arrow-up"></i>', // Text for element
+      activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+    });
+
+
+/* 9. data-background */
+    $("[data-background]").each(function () {
+      $(this).css("background-image", "url(" + $(this).attr("data-background") + ")")
+      });
+
+
+
+
+
+
+
+})(jQuery);
